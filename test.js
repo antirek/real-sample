39186b27d16c53781ab1c5b0d7c6de4544dbbc7f
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-11-10 13:54:00 +0700

    fix

--- test.js
+++ test.js
@@ -1,7 +1,45 @@
 var frexp = require( 'math-float64-frexp' );
 
 
+function fract(f) {
+  return Math.abs(f % 1);
+}
 
-var out = frexp( 4 );
 
-console.log(out);
+var encode = function (number) {
+
+    var arr = new Uint8Array(Buffer.alloc(16, 0));
+
+    arr[0] = 0x01;  //byte with real code asn1
+    arr[1] = 0xFF;  //length
+    arr[2] = 0x12;
+    arr[3] = 0x80;
+
+
+
+
+
+    console.log('arr:', arr);
+    console.log('arr', arr.toString('hex'));
+
+    var out = frexp(number);
+    console.log('out', out);
+ 
+    var mantissa = out[0];
+    var exponenta = out[1];
+
+    console.log('mantissa', mantissa, 'exponenta', exponenta);
+
+    var q = mantissa * (1<<8);
+
+    console.log('mantissa', q);
+
+    var fractional = fract(mantissa * (1<<8));
+
+    console.log('fractional', fractional);
+
+    return 1;
+}
+
+
+encode(5.267899)
